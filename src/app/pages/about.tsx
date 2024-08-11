42325fa12f63b752f2599274f6051d82db2495f3
SUNGHO\andykr <andykr1234@gmail.com>
2024-08-11 21:18:24 +0900

    feat : prettier eslint setting

---
+++ src/app/pages/about.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+
+interface OwnProps {
+    something: string
+}
+
+const About: FC<OwnProps> = () => {
+    return (
+        <div>
+            <h1>11</h1>
+            hello world
+        </div>
+    )
+}
+
+export default About
